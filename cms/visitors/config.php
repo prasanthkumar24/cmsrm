9345473023c7d4995ae75d45ca7bb70b0fd3535a
Prasanth24 <82377359+prasanthkumar24@users.noreply.github.com>
2026-03-13 11:35:39 +0530

    Update config.php
    database cofig changed in visitors

--- cms/visitors/config.php
+++ cms/visitors/config.php
@@ -6,10 +6,15 @@
  */
 
 // Database connection variables
-define('DB_HOST', 'localhost');
-define('DB_USER', 'u764462444_rmdb');
-define('DB_PASS', 'Rmcms@123');
-define('DB_NAME', 'u764462444_rmdb');
+//define('DB_HOST', 'localhost');
+//define('DB_USER', 'u764462444_rmdb');
+//define('DB_PASS', 'Rmcms@123');
+//define('DB_NAME', 'u764462444_rmdb');
+
+define('DB_HOST', getenv('DB_HOST') ?: 'localhost');
+define('DB_USER', getenv('DB_USERNAME') ?: 'u764462444_rmdb');
+define('DB_PASS', getenv('DB_PASSWORD') ?: 'Rmcms@123');
+define('DB_NAME', getenv('DB_DATABASE') ?: 'u764462444_rmdb');
 // Create connection
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
