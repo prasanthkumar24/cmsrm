408d1f38051c8bb08cad76e097817f395cc29dc2
Prasanth24 <82377359+prasanthkumar24@users.noreply.github.com>
2026-03-13 12:28:40 +0530

    Update config.php
    change database name to default

--- cms/leaders/config.php
+++ cms/leaders/config.php
@@ -11,7 +11,7 @@ if (session_status() === PHP_SESSION_NONE) {
 define('DB_HOST', getenv('DB_HOST') ?: 'localhost');
 define('DB_USER', getenv('DB_USERNAME') ?: 'u764462444_rmdb');
 define('DB_PASS', getenv('DB_PASSWORD') ?: 'Rmcms@123');
-define('DB_NAME', getenv('DB_DATABASE') ?: 'u764462444_rmdb');
+define('DB_NAME', getenv('DB_DATABASE') ?: 'default');
 
 
 
