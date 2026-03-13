8c2971bae9e35f49866ddbdf265e0a08e9cd104a
Prasanth24 <82377359+prasanthkumar24@users.noreply.github.com>
2026-03-13 11:34:36 +0530

    Update db.php
    database config changed

--- cms/volunteers/db.php
+++ cms/volunteers/db.php
@@ -3,11 +3,14 @@
 
 // Database Credentials
 // Determine environment
-define('DB_HOST', 'localhost');
-define('DB_USER', 'u764462444_rmdb');
-define('DB_PASS', 'Rmcms@123');
-define('DB_NAME', 'u764462444_rmdb');
-
+//define('DB_HOST', 'localhost');
+//define('DB_USER', 'u764462444_rmdb');
+//define('DB_PASS', 'Rmcms@123');
+//define('DB_NAME', 'u764462444_rmdb');
+define('DB_HOST', getenv('DB_HOST') ?: 'localhost');
+define('DB_USER', getenv('DB_USERNAME') ?: 'u764462444_rmdb');
+define('DB_PASS', getenv('DB_PASSWORD') ?: 'Rmcms@123');
+define('DB_NAME', getenv('DB_DATABASE') ?: 'u764462444_rmdb');
 
 // Establish Connection using PDO
 try {
