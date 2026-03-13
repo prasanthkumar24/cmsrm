80e768a18a07ec27ede4a70e6b483c3c9a353f12
Prasanth24 <82377359+prasanthkumar24@users.noreply.github.com>
2026-03-13 12:30:31 +0530

    Update db.php
    database name change to default

--- cms/volunteers/db.php
+++ cms/volunteers/db.php
@@ -10,7 +10,7 @@
 define('DB_HOST', getenv('DB_HOST') ?: 'localhost');
 define('DB_USER', getenv('DB_USERNAME') ?: 'u764462444_rmdb');
 define('DB_PASS', getenv('DB_PASSWORD') ?: 'Rmcms@123');
-define('DB_NAME', getenv('DB_DATABASE') ?: 'u764462444_rmdb');
+define('DB_NAME', getenv('DB_DATABASE') ?: 'default');
 
 // Establish Connection using PDO
 try {
